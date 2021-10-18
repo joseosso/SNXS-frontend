63b1e34058af11108a4cf972d61021898125fe10
Jose Ossorio <joseossorio99@hotmail.com>
2021-10-18 16:31:54 -0500

    empty pages and axios instance

---
+++ src/pages/Guide/GuidePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GuidePage = () => {
+    return (
+        <div>
+            <h1>Guide Page</h1>
+        </div>
+    );
+};
+
+export default GuidePage;
---
+++ src/pages/ScanResult/ScanResultPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ScanResultPage = () => {
+    return (
+        <div>
+            <h1>Scan result page</h1>
+        </div>
+    );
+};
+
+export default ScanResultPage;
